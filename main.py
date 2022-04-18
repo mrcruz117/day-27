62d2ec7cd85489e3e5f3c862df4c727f841c2358
mrcruz117 <mrcruz1989@gmail.com>
2022-04-18 16:42:12 -0400

    added button and input component

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first Python GUI")
 window.minsize(width=500, height=300)
 
 # Label
 
-my_label = tkinter.Label(text="This is a Label", font=("Roboto",24,"bold"))
-my_label.pack(side="left")
+my_label = Label(text="This is a Label", font=("Roboto", 24, "bold"))
+my_label.pack()
+my_label.config(text="New Text")
+
+
+# button
+def button_click():
+    print("Clicked!")
+    my_label.config(text=f"{input.get()}")
+    input.delete(0, len(input.get()))
+
+
+button = Button(text="click me", command=button_click)
+button.pack()
+
+# Entry
+input = Entry(width=10)
+input.pack()
+print(input.get())
 
 window.mainloop()
